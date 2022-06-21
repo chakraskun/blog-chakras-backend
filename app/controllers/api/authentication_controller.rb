feeb3061ece127110998458ad977410426c078d7
Chakras <chakras.campur@gmail.com>
2022-06-21 11:12:34 +0700

    remove comment

--- app/controllers/api/authentication_controller.rb
+++ app/controllers/api/authentication_controller.rb
@@ -2,7 +2,6 @@ module Api
   class AuthenticationController < ::ApplicationController
     before_action :authorize_request, except: :login
   
-    # POST /auth/login
     def login
       @user = User.find_by_email(params[:email])
       if @user.valid?
